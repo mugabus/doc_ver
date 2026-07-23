94d88fbdae6c6837def25199eefe6b01711207c4
Eddy mugaruka <72892383+mugabus@users.noreply.github.com>
2026-07-23 04:34:04 -0700

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -106,7 +106,7 @@ export function Home() {
 
               <div className="flex items-center space-x-2">
                 <Phone className="w-4 h-4" />
-                <span>+243 971 404 588</span>
+                
               </div>
             </div>
           </div>
